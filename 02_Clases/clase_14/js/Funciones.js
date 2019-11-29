2745b585a66a8cf611ffaacf7b523b92cc8f945e
Lionel Herrero Battista <->
2019-11-29 00:43:59 -0300

    Clase 14

--- 02_Clases/clase_14/js/Funciones.js
+++ 02_Clases/clase_14/js/Funciones.js
@@ -3,17 +3,51 @@ var personas;
     var listaPersonas;
     $("document").ready(CargarPagina);
     function CargarPagina() {
-        //Inicializo lista cuando cargo la página
-        listaPersonas = new Array();
         var btnAgregar = $("#btnAgregar");
         var btnLimpiar = $("#btnLimpiar");
+        var btnModificar = $("#btnModificar");
         var cmbFiltro = $("#cmbFiltro");
-        btnAgregar.click(agregar);
-        btnLimpiar.click(limpiar);
-        cmbFiltro.change(filtrar);
+        var filtrosColumnas = $("input[name='filtro']");
+        //Inicializo lista cuando cargo la página
+        listaPersonas = new Array();
+        //Eventos
+        btnAgregar.click(Agregar);
+        btnLimpiar.click(Limpiar);
+        btnModificar.click(Modificar);
+        cmbFiltro.change(FiltrarPorTipo);
+        filtrosColumnas.change(FiltrarColumnas);
     }
     personas.CargarPagina = CargarPagina;
-    function agregar() {
+    //ABM
+    function Agregar() {
+        var persona;
+        persona = CrearPersona();
+        listaPersonas.push(persona); //variable global
+        CrearTabla(listaPersonas);
+        $('#cmbFiltro option[value="Todos"]').prop('selected', true);
+        FiltrarPorTipo();
+    }
+    personas.Agregar = Agregar;
+    function Eliminar(event) {
+        var fila = $(event.target).parent().parent();
+        var legajo = $(fila.children()[2]).text(); //tomo el legajo de la columna legajo
+        //Elimino la persona de la lista
+        EliminarPersona(listaPersonas, legajo, fila.attr('data-persona-tipo'));
+        fila.remove();
+    }
+    personas.Eliminar = Eliminar;
+    function Modificar() {
+        var tipo = String($("#cmbTipo").find('option:selected').text());
+        var legajo = String($("#txtLegajo").val());
+        var persona = CrearPersona();
+        ModificarPersona(persona, legajo, tipo);
+        CrearTabla(listaPersonas);
+    }
+    personas.Modificar = Modificar;
+    //Fin ABM
+    //Crea a una persona a partir de los datos del inputTxt
+    //Devuelve persona
+    function CrearPersona() {
         var nombre = String($("#txtNombre").val()); //tengo que castear a string
         var apellido = String($("#txtApellido").val());
         var tipo = String($("#cmbTipo").find('option:selected').text()); //busco la opción seleccionada
@@ -21,39 +55,50 @@ var personas;
         if (tipo === 'Alumno') {
             var legajo = String($("#txtLegajo").val());
             persona = new personas.Alumno(nombre, apellido, legajo);
-            listaPersonas.push(persona);
         }
         else {
             var cuil = String($("#txtLegajo").val());
             persona = new personas.Profesor(nombre, apellido, cuil);
-            listaPersonas.push(persona);
         }
-        CrearTabla(persona);
-        filtrar();
+        return persona;
     }
-    personas.agregar = agregar;
-    function eliminar(event) {
-        var fila = $(event.target).parent().parent();
-        //tomo el legajo de la columna legajo
-        var legajo = $(fila.children()[2]).text();
-        //Elimino la persona de la lista
+    personas.CrearPersona = CrearPersona;
+    ///Elimina a una persona por legajo
+    //tipo, por si no hay id único (?)
+    function EliminarPersona(lista, legajo, tipo) {
         listaPersonas.forEach(function (persona, indice) {
             var auxPersona = persona;
             if (persona instanceof personas.Alumno) {
-                if (persona.getLegajo() == legajo && fila.attr('data-persona-tipo') == 'Alumno') {
+                if (persona.getLegajo() == legajo && tipo == 'Alumno') {
                     listaPersonas.splice(indice, 1); //borro ese elemento del array
                 }
             }
             else {
-                if (persona.getCuil() == legajo && fila.attr('data-persona-tipo') == 'Profesor') {
+                if (persona.getCuil() == legajo && tipo == 'Profesor') {
                     listaPersonas.splice(indice, 1);
                 }
             }
         });
-        fila.remove();
     }
-    personas.eliminar = eliminar;
-    function filtrar() {
+    personas.EliminarPersona = EliminarPersona;
+    //Modifica a la persona por legajo
+    //Recibe la persona ya modificada
+    function ModificarPersona(persona, legajo, tipo) {
+        listaPersonas.forEach(function (auxPersona, indice) {
+            if (auxPersona instanceof personas.Alumno) {
+                if (auxPersona.getLegajo() == legajo && tipo == 'Alumno') {
+                    listaPersonas[indice] = persona;
+                }
+            }
+            else {
+                if (auxPersona.getCuil() == legajo && tipo == 'Profesor') {
+                    listaPersonas[indice] = persona;
+                }
+            }
+        });
+    }
+    personas.ModificarPersona = ModificarPersona;
+    function FiltrarPorTipo() {
         var filtro = $("#cmbFiltro").find('option:selected').text();
         var listaFiltrada;
         switch (filtro) {
@@ -71,56 +116,121 @@ var personas;
                 listaFiltrada = listaPersonas;
         }
         $('#tbody').children().remove();
-        listaFiltrada.forEach(function (persona) {
-            CrearTabla(persona);
-        });
+        //Muestro tabla filtrada:
+        CrearTabla(listaFiltrada);
     }
-    personas.filtrar = filtrar;
-    function limpiar() {
+    personas.FiltrarPorTipo = FiltrarPorTipo;
+    function FiltrarColumnas() {
+        var filtro = $("input[name='filtro']:checked").val();
+        if (filtro != undefined) {
+            CrearTabla(listaPersonas);
+        }
+        else //si no está definido ningún valor, no muestro nada
+         {
+            $("#tabla").children().remove();
+        }
+    }
+    personas.FiltrarColumnas = FiltrarColumnas;
+    function Limpiar() {
         $("#txtNombre").val('');
         $("#txtApellido").val('');
         $("#txtLegajo").val('');
     }
-    personas.limpiar = limpiar;
-    function CrearTabla(persona) {
-        var tBody = $("#tbody");
-        var fila = $("<tr></tr>");
-        var columnaNombre = $("<td></td>");
-        columnaNombre.append(persona.getNombre());
-        var columnaApellido = $("<td></td>");
-        columnaApellido.append(persona.getApellido());
-        var columnaLegajo = $("<td></td>");
-        if (persona instanceof personas.Alumno) {
-            columnaLegajo.append(persona.getLegajo());
-            fila.attr('data-persona-tipo', 'Alumno'); //Agrego el tipo como un atributo
-        }
-        else {
-            columnaLegajo.append(persona.getCuil());
-            fila.attr('data-persona-tipo', 'Profesor'); //Agrego el tipo como un atributo
-        }
-        var columnaEliminar = $("<td></td>");
-        var botonEliminar = $("<input></input>");
-        botonEliminar.attr("type", "button");
-        botonEliminar.attr("value", "Eliminar");
-        botonEliminar.attr("class", "btn btn-danger");
-        botonEliminar.click(eliminar);
-        columnaEliminar.append(botonEliminar);
-        fila.append(columnaNombre);
-        fila.append(columnaApellido);
-        fila.append(columnaLegajo);
-        fila.append(columnaEliminar);
-        fila.dblclick(completarTxt);
-        tBody.append(fila);
+    personas.Limpiar = Limpiar;
+    function CrearTabla(lista) {
+        var tabla = $("#tabla");
+        var opcionesMarcadas = $("input[type='checkbox']:checked").get(); //para tomar los elementos del dom
+        var listaDeColumnas = new Array();
+        //Limpio la tabla anterior:
+        tabla.children().remove();
+        //Agrego los filtros seleccionados:
+        opcionesMarcadas.forEach(function (f) {
+            listaDeColumnas.push(String($(f).val()));
+        });
+        //Agrego la columna eliminar a la lista
+        listaDeColumnas.push("Eliminar");
+        CrearThead(listaDeColumnas);
+        CrearBody(lista, listaDeColumnas);
     }
     personas.CrearTabla = CrearTabla;
-    function completarTxt(event) {
+    //Crea el thead
+    //Recibe un array con los nomrbes de las columnas que va a crear
+    function CrearThead(titulosColumnas) {
+        var tabla = $("#tabla");
+        var thead = $("<thead>></thead>");
+        var filahead = $("<tr></tr>");
+        //Creo las columnas
+        var columnasThead = titulosColumnas.map(function (valor) {
+            var th = $("<th></th>");
+            th.attr("scope", "col");
+            th.append(valor);
+            return th;
+        });
+        columnasThead.forEach(function (columna) {
+            filahead.append(columna);
+        });
+        thead.append(filahead);
+        tabla.append(thead);
+    }
+    personas.CrearThead = CrearThead;
+    //Crea un body a partir de una lista de objetos,
+    //ubicando los datos en la columna correspondiente
+    function CrearBody(listaObjetos, listaColumnas) {
+        var tabla = $("#tabla");
+        var tBody = $("<tbody></tbody>");
+        tBody.prop('id', 'tbody');
+        listaObjetos.forEach(function (persona) {
+            var fila = $("<tr></tr>");
+            //Uso map para tomar el valor
+            var columnasTbody = listaColumnas.map(function (valor) {
+                var columna = $("<td></td>"); //Creo la columna
+                //Le paso el valor
+                switch (valor) {
+                    case 'Nombre':
+                        columna.append(persona.getNombre());
+                        break;
+                    case 'Apellido':
+                        columna.append(persona.getApellido());
+                        break;
+                    case 'Legajo':
+                        if (persona instanceof personas.Alumno) {
+                            columna.append(persona.getLegajo());
+                            fila.attr('data-persona-tipo', 'Alumno'); //Agrego el tipo como un atributo
+                        }
+                        else {
+                            columna.append(persona.getCuil());
+                            fila.attr('data-persona-tipo', 'Profesor'); //Agrego el tipo como un atributo
+                        }
+                        break;
+                    case 'Eliminar':
+                        var botonEliminar = $("<input></input>");
+                        botonEliminar.attr("type", "button");
+                        botonEliminar.attr("value", "Eliminar");
+                        botonEliminar.attr("class", "btn btn-danger");
+                        botonEliminar.click(Eliminar);
+                        columna.append(botonEliminar);
+                        break;
+                }
+                return columna;
+            });
+            columnasTbody.forEach(function (columna) {
+                fila.append(columna);
+            });
+            fila.dblclick(CompletarTxt);
+            tBody.append(fila);
+            tabla.append(tBody);
+        });
+    }
+    personas.CrearBody = CrearBody;
+    //Completa el formulario para modificar
+    function CompletarTxt(event) {
         var fila = $(event.target).parent();
         var columnasTabla = fila.children();
-        //completo los txt:
+        //autocompleto los txt del "formulario":
         $("#txtNombre").val($(columnasTabla[0]).html());
         $("#txtApellido").val($(columnasTabla[1]).html());
         $("#txtLegajo").val($(columnasTabla[2]).html());
-        //cambio el cmb:
+        //cambio la opción seleccionada:
         switch (fila.attr('data-persona-tipo')) {
             case 'Alumno':
                 $('#cmbTipo option[value="Alumno"]').prop('selected', true);
@@ -130,5 +240,5 @@ var personas;
                 break;
         }
     }
-    personas.completarTxt = completarTxt;
+    personas.CompletarTxt = CompletarTxt;
 })(personas || (personas = {}));
--- 02_Clases/clase_14/js/Funciones.ts
+++ 02_Clases/clase_14/js/Funciones.ts
@@ -5,23 +5,67 @@ namespace personas
     $("document").ready(CargarPagina);
 
     export function CargarPagina():void{
+   
+        let btnAgregar = $("#btnAgregar");
+        let btnLimpiar = $("#btnLimpiar");
+        let btnModificar = $("#btnModificar");
+        let cmbFiltro = $("#cmbFiltro");
+        let filtrosColumnas = $("input[name='filtro']");
 
         //Inicializo lista cuando cargo la página
         listaPersonas = new Array<Persona>();
-        
-        let btnAgregar = $("#btnAgregar");
-        let btnLimpiar = $("#btnLimpiar");
-        let cmbFiltro = $("#cmbFiltro");
 
-        btnAgregar.click(agregar);
-        btnLimpiar.click(limpiar);
-        cmbFiltro.change(filtrar);
+        //Eventos
+        btnAgregar.click(Agregar);
+        btnLimpiar.click(Limpiar);
+        btnModificar.click(Modificar);
+        cmbFiltro.change(FiltrarPorTipo);
+        filtrosColumnas.change(FiltrarColumnas);
     }
 
-
-    export function agregar()
+    //ABM
+    export function Agregar()
     {
+        let persona:Persona;
 
+        persona = CrearPersona();
+        
+        listaPersonas.push(persona);//variable global
+
+        CrearTabla(listaPersonas);
+        
+        $('#cmbFiltro option[value="Todos"]').prop('selected', true);
+
+        FiltrarPorTipo();
+    }
+
+    export function Eliminar(event)
+    {
+        let fila = $(event.target).parent().parent();
+        let legajo:string = $(fila.children()[2]).text(); //tomo el legajo de la columna legajo
+        
+        //Elimino la persona de la lista
+        EliminarPersona(listaPersonas, legajo, fila.attr('data-persona-tipo'));
+        
+        fila.remove();
+    }
+
+    export function Modificar()
+    {
+        let tipo:string = String($("#cmbTipo").find('option:selected').text())
+        let legajo:string = String($("#txtLegajo").val());
+        let persona:Persona = CrearPersona();
+
+        ModificarPersona(persona, legajo, tipo);
+
+        CrearTabla(listaPersonas);
+    }
+    //Fin ABM
+
+    //Crea a una persona a partir de los datos del inputTxt
+    //Devuelve persona
+    export function CrearPersona():Persona
+    {
         let nombre:string = String($("#txtNombre").val()); //tengo que castear a string
         let apellido:string = String($("#txtApellido").val());    
         let tipo:string = String($("#cmbTipo").find('option:selected').text());//busco la opción seleccionada
@@ -31,69 +75,70 @@ namespace personas
         {
             let legajo:string = String($("#txtLegajo").val());
 
-            persona = new Alumno(nombre, apellido, legajo);
-
-            listaPersonas.push(persona);
+            persona = new Alumno(nombre, apellido, legajo);            
         }
         else
         {
             let cuil:string = String($("#txtLegajo").val());
 
-            persona = new Profesor(nombre, apellido, cuil);
-
-            listaPersonas.push(persona);
+            persona = new Profesor(nombre, apellido, cuil);      
         }
 
-        CrearTabla(persona);
-        
-        filtrar();
-
+        return persona;
     }
 
-
-
-    export function eliminar(event)
+    ///Elimina a una persona por legajo
+    //tipo, por si no hay id único (?)
+    export function EliminarPersona(lista:Array<Persona>, legajo:string, tipo:string)
     {
-        let fila = $(event.target).parent().parent();
-
-        //tomo el legajo de la columna legajo
-        let legajo = $(fila.children()[2]).text();
-
-        //Elimino la persona de la lista
         listaPersonas.forEach(function (persona, indice) {
 
             let auxPersona = persona;
 
             if(persona instanceof Alumno)
             {
-                if((<Alumno>persona).getLegajo() == legajo && fila.attr('data-persona-tipo') == 'Alumno')
+                if((<Alumno>persona).getLegajo() == legajo && tipo == 'Alumno')
                 {
                     listaPersonas.splice(indice, 1);//borro ese elemento del array
-
                 }
-
             }
             else
             {
-                if((<Profesor>persona).getCuil() == legajo && fila.attr('data-persona-tipo') == 'Profesor')
+                if((<Profesor>persona).getCuil() == legajo && tipo == 'Profesor')
                 {
                     listaPersonas.splice(indice, 1);
-
-                }
-
-                 
-            }
-            
-        })
-    
-        fila.remove();
-
+                }             
+            } 
+        });
     }
-    
-    export function filtrar()
+
+    //Modifica a la persona por legajo
+    //Recibe la persona ya modificada
+    export function ModificarPersona(persona:Persona, legajo:string, tipo:string)
+    {
+        listaPersonas.forEach(function (auxPersona, indice) {
+
+            if(auxPersona instanceof Alumno)
+            {
+                if((<Alumno>auxPersona).getLegajo() == legajo && tipo == 'Alumno')
+                {                   
+                    listaPersonas[indice] = persona;
+                }
+            }
+            else
+            {
+                if((<Profesor>auxPersona).getCuil() == legajo && tipo == 'Profesor')
+                {
+                    listaPersonas[indice] = persona;
+                }          
+            }          
+        });
+    }
+
+    export function FiltrarPorTipo()
     {
         let filtro = $("#cmbFiltro").find('option:selected').text();
-        let listaFiltrada;
+        let listaFiltrada:Array<Persona>;
 
         switch(filtro)
         {
@@ -101,14 +146,12 @@ namespace personas
                 listaFiltrada = listaPersonas.filter(function (persona) {
                     
                     return persona instanceof Alumno;
-
                 });
                 break;
             case 'Profesores':
                 listaFiltrada = listaPersonas.filter(function (persona) {
                 
                     return persona instanceof Profesor;
-
                 });
                 break;
             case 'Todos':
@@ -117,74 +160,164 @@ namespace personas
 
         $('#tbody').children().remove();
 
-        listaFiltrada.forEach(function(persona) {
+        //Muestro tabla filtrada:
+        CrearTabla(listaFiltrada);
+    }
+   
+    export function FiltrarColumnas()
+    {
+        let filtro = $("input[name='filtro']:checked").val();
 
-            CrearTabla(persona);
-
-        });
-
+        if(filtro != undefined)
+        {
+            CrearTabla(listaPersonas);
+        }
+        else //si no está definido ningún valor, no muestro nada
+        {
+            $("#tabla").children().remove();
+        }
     }
 
-    export function limpiar()
+    export function Limpiar()
     {
         $("#txtNombre").val(''); 
         $("#txtApellido").val('');    
         $("#txtLegajo").val('');
     }
 
-
-    export function CrearTabla(persona)
+    export function CrearTabla(lista)
     {
-        let tBody = $("#tbody");
-        let fila = $("<tr></tr>");
 
-        let columnaNombre = $("<td></td>");
-        columnaNombre.append(persona.getNombre());
+        let tabla = $("#tabla");
+        let opcionesMarcadas = $("input[type='checkbox']:checked").get();//para tomar los elementos del dom
+        let listaDeColumnas:Array<string> = new Array();
 
-        let columnaApellido = $("<td></td>");
-        columnaApellido.append(persona.getApellido());
+        //Limpio la tabla anterior:
+        tabla.children().remove();
 
-        let columnaLegajo = $("<td></td>");  
-        if(persona instanceof Alumno)
-        {
-            columnaLegajo.append((<Alumno>persona).getLegajo());
-            fila.attr('data-persona-tipo', 'Alumno');//Agrego el tipo como un atributo
-        }
-        else
-        {
-            columnaLegajo.append((<Profesor>persona).getCuil());
-            fila.attr('data-persona-tipo', 'Profesor');//Agrego el tipo como un atributo
-        }
-
-        let columnaEliminar = $("<td></td>");
-        let botonEliminar = $("<input></input>");
-        botonEliminar.attr("type", "button");
-        botonEliminar.attr("value", "Eliminar");
-        botonEliminar.attr("class", "btn btn-danger");
-        botonEliminar.click(eliminar);
-        columnaEliminar.append(botonEliminar);
- 
-        fila.append(columnaNombre);
-        fila.append(columnaApellido);
-        fila.append(columnaLegajo);
-        fila.append(columnaEliminar);
-
-        fila.dblclick(completarTxt);
+        //Agrego los filtros seleccionados:
+        opcionesMarcadas.forEach(function (f) {
+                
+            listaDeColumnas.push(String($(f).val()));
+            
+        });
         
-        tBody.append(fila);
+        //Agrego la columna eliminar a la lista
+        listaDeColumnas.push("Eliminar");
+
+        CrearThead(listaDeColumnas);
+    
+        CrearBody(lista, listaDeColumnas);
     }
 
-    export function completarTxt(event)
+    //Crea el thead
+    //Recibe un array con los nomrbes de las columnas que va a crear
+    export function CrearThead(titulosColumnas:Array<String>)
+    {
+        let tabla = $("#tabla");
+        let thead = $("<thead>></thead>");
+        let filahead = $("<tr></tr>");
+
+        //Creo las columnas
+        let columnasThead = titulosColumnas.map( function(valor:string) {
+
+            let th = $("<th></th>");
+            th.attr("scope", "col");
+            th.append(valor);
+
+            return th;
+
+        });
+
+        columnasThead.forEach(function(columna) {
+
+            filahead.append(columna);
+        });
+
+        thead.append(filahead);
+        tabla.append(thead);
+    }
+
+    //Crea un body a partir de una lista de objetos,
+    //ubicando los datos en la columna correspondiente
+    export function CrearBody(listaObjetos:Array<Persona>, listaColumnas:Array<String>)
+    {
+        let tabla = $("#tabla");
+        let tBody = $("<tbody></tbody>");
+        tBody.prop('id', 'tbody');
+
+        listaObjetos.forEach( function(persona) {
+
+            let fila = $("<tr></tr>");
+
+            //Uso map para tomar el valor
+            let columnasTbody = listaColumnas.map( function(valor) {
+
+                let columna = $("<td></td>");//Creo la columna
+
+                //Le paso el valor
+                switch(valor)
+                {
+                    case 'Nombre': 
+                    columna.append(persona.getNombre());
+                    break;
+
+                    case 'Apellido':
+                    columna.append(persona.getApellido());
+                    break;
+
+                    case 'Legajo':                     
+                    if(persona instanceof Alumno)
+                    {
+                        columna.append((<Alumno>persona).getLegajo());
+                        fila.attr('data-persona-tipo', 'Alumno');//Agrego el tipo como un atributo
+                    }
+                    else
+                    {
+                        columna.append((<Profesor>persona).getCuil());
+                        fila.attr('data-persona-tipo', 'Profesor');//Agrego el tipo como un atributo
+                    }
+                    break;
+                
+                    case 'Eliminar':                  
+                    let botonEliminar = $("<input></input>");
+                    botonEliminar.attr("type", "button");
+                    botonEliminar.attr("value", "Eliminar");
+                    botonEliminar.attr("class", "btn btn-danger");
+                    botonEliminar.click(Eliminar);
+                    columna.append(botonEliminar);
+                    break;
+
+                }
+                
+                return columna;
+            });
+
+            columnasTbody.forEach(function(columna) {
+
+                fila.append(columna);
+            });
+
+            fila.dblclick(CompletarTxt);
+            tBody.append(fila);
+            tabla.append(tBody);
+
+        });
+
+    }
+
+    //Completa el formulario para modificar
+    export function CompletarTxt(event)
     {
         let fila = $(event.target).parent();
         let columnasTabla = fila.children();
         
-        //completo los txt:
+        //autocompleto los txt del "formulario":
         $("#txtNombre").val($(columnasTabla[0]).html()); 
         $("#txtApellido").val($(columnasTabla[1]).html());    
         $("#txtLegajo").val($(columnasTabla[2]).html());
 
-        //cambio el cmb:
+        //cambio la opción seleccionada:
         switch(fila.attr('data-persona-tipo'))
         {
             case 'Alumno':
@@ -194,6 +327,5 @@ namespace personas
                 $('#cmbTipo option[value="Profesor"]').prop('selected', true);
                 break;
         }
-
     }
 }
